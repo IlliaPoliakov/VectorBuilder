46c87a96ee6cef680ae9d8f97fa8280d979ecfdf
Illia_Poliakov <illiapoliakov1@gmail.com>
2022-11-30 17:57:21 +0300

    hide keyboard extension for uiViewController

--- VectorBuilder/VectorBuilder/App/Extensions.swift
+++ VectorBuilder/VectorBuilder/App/Extensions.swift
@@ -134,3 +134,16 @@ extension UIColor {
         )
     }
 }
+
+
+extension UIViewController {
+  func hideKeyboardWhenTappedAround() {
+    let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+    tap.cancelsTouchesInView = false
+    view.addGestureRecognizer(tap)
+  }
+  
+  @objc func dismissKeyboard() {
+    view.endEditing(true)
+  }
+}
--- VectorBuilder/VectorBuilder/UI/Presenters/SideBarPresenter.swift
+++ VectorBuilder/VectorBuilder/UI/Presenters/SideBarPresenter.swift
@@ -75,7 +75,40 @@ final class SideBarPresenter: NSObject, SideBarPresenterProtocol {
   func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     let vector = mainPresenter!.vectors[indexPath.row]
     
-    mainPresenter?.moveScrollViewToPoint(
-      vector.startPoint.centerPoint(withPoint: vector.endPoint))
+    var point = vector.endPoint
+    point.x += self.viewController!.view.frame.width / 6
+    mainPresenter?.moveScrollViewToPoint(point)
+    vector.highlight()
+  }
+}
+
+
+extension SideBarViewController: UIContextMenuInteractionDelegate {
+  func contextMenuInteraction(_ interaction: UIContextMenuInteraction,
+                              configurationForMenuAtLocation location: CGPoint)
+  -> UIContextMenuConfiguration? {
+    
+    return UIContextMenuConfiguration(
+      identifier: nil,
+      previewProvider: nil,
+      actionProvider: { _ in
+        let deleteAction = self.deleteAction()
+        let children: [UIMenuElement] = [deleteAction]
+        return UIMenu(title: "", children: children)
+      })
+  }
+  
+  func deleteAction() -> UIAction {
+    let allAttributes = UIMenuElement.Attributes()
+    let trashImage = UIImage(systemName: "trash")!
+      .withTintColor(Colors.mainColorClear, renderingMode: .alwaysOriginal)
+    
+    return UIAction(
+      title: "Delete",
+      image: trashImage,
+      identifier: nil,
+      attributes: allAttributes) { _ in
+        presenter.
+      }
   }
 }
